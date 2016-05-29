bc21a93d35e5be38fe0f2941a8a79ba92ee27e41
strothj <strothjs@gmail.com>
2016-05-29 04:34:33 -0400

    added package description

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package debrepo provides a Debian Repository client library for Go.
+// It is based on the published structure of Debian style repositories:
+// https://wiki.debian.org/RepositoryFormat
+package debrepo
